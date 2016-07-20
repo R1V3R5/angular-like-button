2e49bc7e592c7a380e482487562258047c34f77c
Joshua Rivers <joshua.rivers@gmail.com>
2016-07-20 09:46:35 -0400

    fixed bad practice to good practice

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -1,19 +1,27 @@
 function MainController ($scope) {
+ 
+  $scope.count = 0;
+  $scope.text  = 'Likes';
 
-   
-  $scope.bumpCount = function (count) {
-    
-    if (count === 0) {
-      return count + ' Likes';
-    } else if (count === 1) {
-      return count + ' Like'
-    } else if (count >= 2) {
-      return count + ' Likes'
-    }
+  $scope.bumpCount = () => {
+    $scope.count ++;
+    $scope.text = ($scope.count === 1) ? "Like" : "Likes";
+
   }
+
+    
+
+  //   if (count === 0) {
+  //     return count + ' Likes';
+  //   } else if (count === 1) {
+  //     return count + ' Like'
+  //   } else if (count >= 2) {
+  //     return count + ' Likes'
+  //   }
+  // }
 
 
 }
 
-MainController.$inject = ['$scope']
+MainController.$inject = ['$scope'];
 export { MainController };
